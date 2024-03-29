0d82ed061c440aed8853b0f2fbb575faf5c229ee
D4RKM4773R <jayalaa1900@alumno.ipn.mx>
2024-03-28 21:49:03 -0600

    cambio metodo de conexion de CURL a no se

--- assets/php/procesar_actualizar.php
+++ assets/php/procesar_actualizar.php
@@ -27,46 +27,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Convertir los datos a JSON
     $data_json = json_encode($datos);
 
-    // Inicializar cURL
-    $ch = curl_init($supabaseUrl);
+    // Configurar la solicitud HTTP a Supabase para insertar datos
+    $options = array(
+        'http' => array(
+            'header' => "Content-Type: application/json\r\n" .
+                        "apikey: $supabaseKey\r\n" .
+                        "Prefer: return=representation\r\n", // Header para que Supabase devuelva los datos de la fila insertada
+            'method' => 'PATCH',
+            'content' => $data_json, // Datos a insertar
+        ),
+    );
 
-    //Configurar la solicitud
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH'); // Método de solicitud PATCH para actualizar
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json); // Datos a actualizar
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Devolver el resultado como cadena
+    // Realizar la solicitud HTTP
+    $context = stream_context_create($options);
+    $response = @file_get_contents($supabaseUrl, false, $context);
 
-    // Establecer los encabezados de la solicitud
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Type: application/json',
-        'apikey: ' . $supabaseKey,
-        'Prefer: return=representation' // Header para que Supabase devuelva los datos de la fila actualizada
-    ));
-
-    // Ejecutar la solicitud y guardar la respuesta en una variable
-    $response = curl_exec($ch);
-    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-    // Verificar si hubo algún error en la solicitud
-    if(curl_errno($ch)) {
-        // Manejar el error de solicitud HTTP
-        echo "Error en la solicitud HTTP: " . $httpCode;
-        echo "Error CURL: " . curl_error($ch);
-    } else {
-        // Verificar el código de respuesta HTTP
-        if ($httpCode >= 200 && $httpCode < 300) {
-            // Procesar la respuesta recibida en caso de éxito
-            echo "Actualización exitosa";
-        } else {
-            // Manejar respuestas HTTP que indican error
-            echo "Error en la solicitud HTTP: " . $httpCode;
+    if ($response === FALSE) {
+        if (isset($http_response_header)) {
+            echo "Headers de respuesta: \n";
+            print_r($http_response_header);
         }
+        echo "Error al realizar la solicitud HTTP.";
+    } else {
+        //echo $response;
+        echo "Inserción exitosa";
     }
-
-    // Cerrar la sesión cURL
-    curl_close($ch);
-
-    // Procesar la respuesta recibida
-    echo "Actualización exitosa";
 } else {
     echo "<h2>Método de solicitud no soportado.</h2>";
 }
