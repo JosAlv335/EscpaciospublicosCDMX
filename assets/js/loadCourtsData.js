b1c952737147f0caf1f907604ac851888d7e5967
D4RKM4773R <jayalaa1900@alumno.ipn.mx>
2024-03-29 21:51:26 -0600

    Script para insertar todos los datos del archivo courtsData.js en los campos select determinados

---
+++ assets/js/loadCourtsData.js
@@ -0,0 +1,38 @@
+/*
+SCRIPT - loadcourtsData.js
+SCRIPT DISEÑADO PARA CARGAR OPCIONES DENTRO DE UN SELECT
+
+INICIALMENTE DISEÑADO PARA USARSE EN:
+    -addCourt.html
+    En conjunto con
+    -courtsData.js
+*/
+
+/*************************************************************************************************************/
+/*********************************DECLARACIÓN DE LOS USOS EN EL SISTEMA***************************************/
+/*************************************************************************************************************/
+// Obtener elementos select
+const tipoInstalacionSelect = document.getElementById('tipoInstalacion');
+const deportePrincipalSelect = document.getElementById('deporte-principal');
+
+// Cargar opciones
+cargarOpciones(tipoInstalacionSelect, tiposDeInstalacion);
+cargarOpciones(deportePrincipalSelect, deportesPrincipales);
+
+/*
+FUNCIÓN cargarOpciones(selectElement, opciones)
+ARGUMENTOS:
+    -selectelement      -       Variable que tiene la información de un elemento <select> al cual insertarle las opciones
+    -opciones           -       arreglo con las distintas opciones a agregar
+*/
+function cargarOpciones(selectElement, opciones) {
+    opciones.forEach(opcion => {
+      const option = document.createElement('option');
+      option.text = opcion;
+      option.value = opcion;
+      selectElement.appendChild(option);
+    });
+  }
+  
+
+  
