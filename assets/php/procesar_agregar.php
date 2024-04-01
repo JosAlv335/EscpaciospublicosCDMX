5e7516bcaef9365dc98f42aaee9d8f895c301d6d
D4RKM4773R <jayalaa1900@alumno.ipn.mx>
2024-03-31 23:48:29 -0600

    Imprime los datos que se guardaron

--- assets/php/procesar_agregar.php
+++ assets/php/procesar_agregar.php
@@ -68,6 +68,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($http_response_header)) {
             echo "Headers de respuesta: \n";
             print_r($http_response_header);
+            echo "Datos: \n";
+            print_r($$datos);
         }
         echo "Error al realizar la solicitud HTTP.";
     } else {
